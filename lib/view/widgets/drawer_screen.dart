c1a4ad13ae85c4b5e27aa739f947242ffe186cd1
Dipika Ranabhat <qubeena7@gmail.com>
2022-06-22 13:12:56 +0545

    drawer change

--- lib/view/widgets/drawer_screen.dart
+++ lib/view/widgets/drawer_screen.dart
@@ -20,76 +20,79 @@ class DrawerScreen extends StatelessWidget {
   }
 
   Widget buildHeader(BuildContext context) => Padding(
-        padding: const EdgeInsets.all(8.0),
+        padding: const EdgeInsets.all(10.0),
         child: Container(
+          padding: const EdgeInsets.all(5),
           // height: MediaQuery.of(context).size.height,
           decoration: BoxDecoration(
               borderRadius: BorderRadius.circular(10),
               color: const Color.fromARGB(255, 173, 206, 232),
               boxShadow: [
                 BoxShadow(
-                  color: Colors.grey.withOpacity(0.5), //color of shadow
+                  color: const Color.fromARGB(255, 0, 0, 0)
+                      .withOpacity(0.5), //color of shadow
                   spreadRadius: 5, //spread radius
                   blurRadius: 7, // blur radius
                   offset: const Offset(0, 2), // changes position of shadow
                 ),
               ]),
           // padding: const EdgeInsets.only(top: 10),
-          child: Column(children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                IconButton(onPressed: (() {}), icon: const Icon(Icons.edit)),
-              ],
-            ),
-            const CircleAvatar(
+          child: Column(children: const [
+            // Row(
+            //   mainAxisAlignment: MainAxisAlignment.end,
+            //   children: [
+            //     IconButton(onPressed: (() {}), icon: const Icon(Icons.edit)),
+            //   ],
+            // ),
+
+            CircleAvatar(
               radius: 60,
               backgroundImage: NetworkImage(
                   "https://cdn.dribbble.com/users/2364329/screenshots/10481283/050.jpg"),
             ),
-            const SizedBox(
+            SizedBox(
               height: 10,
             ),
-            const Text(
+            Text(
               "Admin",
               style: TextStyle(
                 fontSize: 18,
                 fontWeight: FontWeight.bold,
               ),
             ),
-            const SizedBox(
+            SizedBox(
               height: 8,
             ),
-            const Text(
+            Text(
               "admin@gmail.com",
               style: TextStyle(
                   fontSize: 16,
                   fontStyle: FontStyle.italic,
                   fontWeight: FontWeight.normal),
             ),
-            const SizedBox(
+            SizedBox(
               height: 8,
             ),
-            const Text(
+            Text(
               "9876543210",
               style: TextStyle(
                   fontSize: 16,
                   fontStyle: FontStyle.italic,
                   fontWeight: FontWeight.normal),
             ),
-            const SizedBox(
+            SizedBox(
               height: 8,
             ),
-            const Text(
+            Text(
               "Company Name: Dynamic Technosoft",
               style: TextStyle(
                   fontSize: 16,
                   // fontStyle: FontStyle.italic,
                   fontWeight: FontWeight.normal),
             ),
-            const SizedBox(
-              height: 8,
-            ),
+            // SizedBox(
+            //   height: 8,
+            // ),
           ]),
         ),
       );
